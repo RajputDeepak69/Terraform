be52101ebc393a2e6bf8eaf46edc82934a715968
RajpootDeepak <161629677+RajputDeepak69@users.noreply.github.com>
2026-08-19 00:00:55 +0530

    module lab

---
+++ labs/module-lab/variables.tf
@@ -0,0 +1,9 @@
+variable "ami-id" {
+    description = "ami-id for creating instances"
+    type = string
+}
+
+variable "instance_type" {
+    description = "instane type for VMs"
+    type = string
+}

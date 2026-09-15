2b41be54b172c879f05e2cbd2344490b1ee0b25a
RajpootDeepak <161629677+RajputDeepak69@users.noreply.github.com>
2026-09-16 00:29:29 +0530

    vault integration lab added

---
+++ labs/vault-integration-lab/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+    required_providers {
+      aws = {
+        source = "hashicorp/aws"
+        version = "6.57.1"
+      }
+    }
+}
+
+provider "aws" {
+    alias = "default"
+    region = "us-east-1"
+}

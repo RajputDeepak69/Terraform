53a98a9a0949bd9508b6b26a069063d9b3d764ee
RajpootDeepak <161629677+RajputDeepak69@users.noreply.github.com>
2026-09-18 23:59:32 +0530

    added vault in tf

--- labs/vault-integration-lab/provider.tf
+++ labs/vault-integration-lab/provider.tf
@@ -4,10 +4,18 @@ terraform {
         source = "hashicorp/aws"
         version = "6.57.1"
       }
+      vault = {
+        source = "hashicorp/vault"
+        version = "v2.1.0"
+      }
     }
 }
 
 provider "aws" {
     alias = "default"
     region = "us-east-1"
+}
+provider "vault" {
+  address = var.address
+  token = var.token
 }
---
+++ labs/vault-integration-lab/variables.tf
@@ -0,0 +1,6 @@
+variable "address" {
+    description = "this is for address and port for vault"
+}
+variable "token" {
+    description = "this one for the token ...."
+}
